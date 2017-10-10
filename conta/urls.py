228f82c79cba787c6425fdf78afba1160597ffdd
Luis <jinchuika@gmail.com>
2017-10-10 08:44:17 -0600

    Docs

--- conta/urls.py
+++ conta/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.views import static
 
 urlpatterns = [
+    url(r'^docs/$', static.serve, {'document_root': settings.DOCS_ROOT, 'path': 'search.html'}),
+    url(r'^docs/(?P<path>.+)$', static.serve, {'document_root': settings.DOCS_ROOT}),
     url(r'^bancos/', include('bancos.urls')),
     url(r'^inventario/', include('inventario.urls')),
     url(r'^contabilidad/', include('contabilidad.urls')),
--- docs/conf.py
+++ docs/conf.py
@@ -89,13 +89,14 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'haiku'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 # html_theme_options = {}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -107,15 +108,14 @@ html_static_path = ['_static']
 #
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-        'donate.html',
-    ]
-}
+# html_sidebars = {
+#     '**': [
+#         'navigation.html',
+#         'relations.html',  # needs 'show_related': True theme option to display
+#         'searchbox.html',
+#         'donate.html',
+#     ]
+# }
 
 
 # -- Options for HTMLHelp output ------------------------------------------
